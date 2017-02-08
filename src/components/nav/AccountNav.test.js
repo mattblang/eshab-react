bd3f14c33338de848adfd5db0000a90336a68f9c
Matt Langston <matt@mattblang.com>
2017-02-07 22:04:23 -0600

    Implement first Jest test

--- src/components/nav/AccountNav.test.js
+++ src/components/nav/AccountNav.test.js
@@ -1,12 +1,39 @@
+// @flow
+
 import React from 'react'
 import renderer from 'react-test-renderer'
 import AccountNav from './AccountNav'
 import AccountType from '../models/AccountType'
+import Account from '../models/Account'
+import * as firebase from 'firebase'
+
+jest.mock('firebase', () => {
+    return {
+        database: () => {
+            return {
+                ref: () => {
+                    return {
+                        orderByChild: () => {
+                            return {
+                                equalTo: () => {
+                                    return {
+                                        on: () => {
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+})
 
 it('renders correctly', () => {
     const component = renderer.create(
         <AccountNav type={AccountType.BUDGET} />
     )
-
     expect(component.toJSON()).toMatchSnapshot();
 })
